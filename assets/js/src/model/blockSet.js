a1722c326b29111f3ff8f04c89d913dc4d4f4ea9
mavenlink <jhurst@mavenlink.com>
2019-03-29 15:12:00 -0600

    Add BlockSet

---
+++ assets/js/src/model/blockSet.js
@@ -0,0 +1,73 @@
+/**
+ * blockSet.js
+ *
+ * Object that handles where blocks are and removing lines and junk. Also only handles things in grid units.
+ */
+export class BlockSet {
+	constructor(width, height) {
+		this.dimensions = { width, height };
+		this.blocks = new Map();
+	}
+
+	/** Clears lines out and returns the number of lines cleared. */
+	clearLines() {
+		let lines = 0;
+
+		for (let y = this.dimensions.height - 1; y >= 0; y--) {
+			let rowHasLine = true;
+
+			for (let x = 0; x < this.dimensions.width; x++) {
+				if (!this.hasBlockAt({ x, y })) {
+					rowHasLine = false;
+					break;
+				}
+			}
+
+			if (rowHasLine) {
+				lines += 1;
+			} else {
+				this._shiftLines(y);
+				break;
+			}
+		}
+
+		return { lines };
+	}
+
+	/** Places all the blocks at the locations associated. Must be an array of positions. */
+	placeBlocks(blocks) {
+		blocks.forEach(b => placeBlockAt(b));
+	}
+
+	/** Places a block at the location provided. */
+	placeBlockAt(position) {
+		if (this._positionInBounds(position) && !this.hasBlockAt(position)) {
+			const xVals = this.blocks.get(position.y) || new Set();
+			xVals.add(position.x);
+			this.blocks.set(position.y, xVals);
+		}
+	}
+
+	/** Returns true if the block set has a block at the location provided. */
+	hasBlockAt(position) {
+		const xVals = this.blocks.get(position.y);
+		return xVals && xVals.has(position.x);
+	}
+
+	/*---PRIVATE */
+
+	/** Helper method. Shifts all blocks down to the bottom from the row provided. */
+	_shiftLines(row) {
+		const diffY = (this.dimensions.height - 1) - row;
+		for (let y = row; y >= 0; y--) {
+			const xVals = this.blocks.get(y);
+			this.blocks.set(y + diffY, xVals);
+			this.blocks.delete(y);
+		}
+	}
+
+	/** Helper method. Returns whether or not the position provided is in bounds. */
+	_positionInBounds(position) {
+		return position.x < this.dimensions.width && position.x >= 0 && position.y < this.dimensions.height && position.y >= 0;
+	}
+}
---
+++ assets/js/test/model/blockSetTest.js
@@ -0,0 +1,57 @@
+/**
+ * blockSetTest.js
+ */
+import { BlockSet } from '../../src/model/blockSet.js';
+
+QUnit.module('BlockSet', () => {
+	QUnit.module('#placeBlockAt', () => {
+		QUnit.test('allows placement of a block', (assert) => {
+			const blockSet = new BlockSet(10, 10);
+			blockSet.placeBlockAt({ x: 3, y: 9 });
+			assert.ok(blockSet.hasBlockAt({ x: 3, y: 9 }));
+		});
+
+		QUnit.test('does not allow placement of a block out of bounds', (assert) => {
+			const blockSet = new BlockSet(10, 10);
+			blockSet.placeBlockAt({ x: 10, y: 9 });
+			assert.notOk(blockSet.hasBlockAt({ x: 10, y: 9 }));
+		});
+	});
+
+	QUnit.module('#clearLines', () => {
+		QUnit.test('returns 1 when one line is cleared', (assert) => {
+			const blockSet = new BlockSet(3, 10);
+			blockSet.placeBlockAt({ x: 0, y: 9 });
+			blockSet.placeBlockAt({ x: 1, y: 9 });
+			blockSet.placeBlockAt({ x: 2, y: 9 });
+			
+			const result = blockSet.clearLines();
+			assert.equal(result.lines, 1);
+			assert.notOk(blockSet.hasBlockAt({ x: 0, y: 9 }));
+			assert.notOk(blockSet.hasBlockAt({ x: 1, y: 9 }));
+			assert.notOk(blockSet.hasBlockAt({ x: 2, y: 9 }));
+		});
+
+		QUnit.test('returns zero when no lines are cleared', (assert) => {
+			const blockSet = new BlockSet(3, 10);
+			blockSet.placeBlockAt({ x: 0, y: 9 });
+			blockSet.placeBlockAt({ x: 1, y: 9 });
+			
+			const result = blockSet.clearLines();
+			assert.equal(result.lines, 0);
+		});
+
+		QUnit.test('returns 3 when three lines are cleared', (assert) => {
+			const blockSet = new BlockSet(2, 10);
+			blockSet.placeBlockAt({ x: 0, y: 9 });
+			blockSet.placeBlockAt({ x: 1, y: 9 });
+			blockSet.placeBlockAt({ x: 0, y: 8 });
+			blockSet.placeBlockAt({ x: 1, y: 8 });
+			blockSet.placeBlockAt({ x: 0, y: 7 });
+			blockSet.placeBlockAt({ x: 1, y: 7 });
+			
+			const result = blockSet.clearLines();
+			assert.equal(result.lines, 3);
+		});
+	});
+});
