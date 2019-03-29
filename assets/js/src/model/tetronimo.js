89fdd95cdd8feca9ebb534108e78b603493aee90
mavenlink <jhurst@mavenlink.com>
2019-03-29 15:46:13 -0600

    Remove dead code

--- assets/js/src/model/tetronimo.js
+++ assets/js/src/model/tetronimo.js
@@ -104,20 +104,6 @@ export class Tetronimo {
 		}
 	}
 
-	/** Helper method. Returns whether or not the position provided respects the boundary provided in the movement provided. */
-	_respectsBoundary(position, movement, boundary) {
-		switch(movement) {
-			case Tetronimo.LEFT:
-				return position.x >= boundary;
-			case Tetronimo.RIGHT:
-				return position.x <= boundary;
-			case Tetronimo.DOWN:
-				return position.y <= boundary;
-			case Tetronimo.ROTATE:
-				return position.x >= boundary.left && position.x <= boundary.right && position.y <= boundary.down;
-		}
-	}
-
 	/*---PRIVATE */
 	_getDiff(oldPosition, newPosition) {
 		return { x: newPosition.x - oldPosition.x, y: newPosition.y - oldPosition.y };
