e186c8ea0aa403e7581f9858f86e687cb9ec955a
mavenlink <jhurst@mavenlink.com>
2019-03-29 16:10:53 -0600

    Add DeadBlocks service

---
+++ assets/js/src/service/deadBlocks.js
@@ -0,0 +1,34 @@
+/**
+ * deadBlocks.js
+ *
+ * Object that handles what blocks hit the floor (i.e. are "dead") and offers methods to grab them to
+ * be cleaned up by the GameplayScene.
+ */
+export class DeadBlocks {
+	constructor() {
+		this.deadBlocks = [];
+	}
+
+	/** Removes all blocks that match the positions provided in the list provided. Returns the block objects included. */
+	blocksMatching(positionList) {
+		const blocksToClean = [];
+
+		positionList.forEach((pos) => {
+			this.deadBlocks.forEach((block) => {
+				const blockPos = block.getPosition();
+
+				if (pos.x === blockPos.x && pos.y === blockPos.y) {
+					blocksToClean.push(block);
+				}
+			});
+		});
+
+		this.deadBlocks = this.deadBlocks.filter(b => blocksToClean.includes(b));
+		return blocksToClean;
+	}
+
+	/** Adds the list of blocks provided to the collection of dead blocks. */
+	addBlocks(blocks) {
+		this.deadBlocks = this.deadBlocks.concat(blocks);
+	}
+}
---
+++ assets/js/test/service/deadBlocksTest.js
@@ -0,0 +1,18 @@
+/**
+ * deadBlocksTest.js
+ */
+import { DeadBlocks } from '../../src/service/deadBlocks.js';
+import { Block } from '../../src/model/block.js';
+
+QUnit.module('DeadBlocks', () => {
+	QUnit.test('properly maintains what blocks to grab', (assert) => {
+		const deadBlocks = new DeadBlocks();
+		const blockOne = new Block({ x: 0, y: 100 });
+		const blockTwo = new Block({ x: 32, y: 100 });
+
+		deadBlocks.addBlocks([ blockOne, blockTwo ]);
+		const blocksToClean = deadBlocks.blocksMatching([{ x: 0, y: 100 }]);
+		assert.ok(blocksToClean.includes(blockOne));
+		assert.notOk(blocksToClean.includes(blockTwo));
+	});
+});
